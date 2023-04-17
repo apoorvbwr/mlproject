1cb5336395a3dab6b4650364f54061d337d78e88
Apoorv Sharma <apoorvsharmabwr@gmail.com>
2023-04-17 19:19:04 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(name="mlproject",
+      version="0.0.1",
+      author="Apoorv",
+      author_email = "apoorvbwr@gmail.com",
+      packages=find_packages(),
+      install_requires=["pandas", "numpy", "seaborn"])
